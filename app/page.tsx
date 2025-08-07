73348146e374e9630476eee7e0940cc818bb7003
Halil Utku Demirtaş <halil.demirtas@ogr.gidatarim.edu.tr>
2025-08-07 11:37:49 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -829,7 +829,7 @@ export default function Portfolio() {
               </CardHeader>
               <CardContent>
                 <audio controls className="w-full mb-4">
-                  <source src="/Whisper of the Ent's.m4a" type="audio/mp4" />
+                  <source src="app/Whisper of the Ent's.m4a" type="audio/mp4" />
                   Your browser does not support the audio element.
                 </audio>
                 <div className="flex flex-wrap gap-2">
@@ -853,7 +853,7 @@ export default function Portfolio() {
               </CardHeader>
               <CardContent>
                 <audio controls className="w-full mb-4">
-                  <source src="/Haunted House.m4a" type="audio/mp4" />
+                  <source src="app/Haunted House.m4a" type="audio/mp4" />
                   Your browser does not support the audio element.
                 </audio>
                 <div className="flex flex-wrap gap-2">
